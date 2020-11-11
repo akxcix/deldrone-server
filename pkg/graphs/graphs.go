b82a364931e11c9d1adebc40342d9d315b953a21
Adarsh <contactadarshk@gmail.com>
2020-11-11 16:39:30 +0530

    add DFS method and corresponding test

--- pkg/graphs/graphs.go
+++ pkg/graphs/graphs.go
@@ -6,13 +6,16 @@ import (
 )
 
 var (
-	// ErrNodeDoesNotExist is used when an operation is performed on a node which is not present in the graph
+	// ErrNodeDoesNotExist is used when an operation is performed on a node which is not present
+	// in the graph
 	ErrNodeDoesNotExist = errors.New("graphs: operation performed on a Node which does not exist in Graph")
+
+	// ErrNegativeEdgeWeight is used when the graph is provided a negative edge weight
+	ErrNegativeEdgeWeight = errors.New("graphs: negative edge weight in graph")
 )
 
-// Node is an interface that represents any item that can be put into the graph
-type Node interface {
-}
+// Node is an empty interface that represents any item that can be put into the graph
+type Node interface{}
 
 // Graph implements a directed weighted graph using an adjacency matrix using nested hashmaps
 type Graph struct {
@@ -31,14 +34,35 @@ func (g *Graph) Insert(x Node) {
 
 // Connect connects Node a to Node b with a weight
 func (g *Graph) Connect(a Node, b Node, weight int) error {
+	// check if negative egde weight is provided to graph. If so, return error
+	if weight < 0 {
+		return ErrNegativeEdgeWeight
+	}
+
 	// check if a is present in the adjacency matrix. if not return error
-	_, ok := g.adjacencyMatrix[a]
-	if !ok {
+	_, ok1 := g.adjacencyMatrix[a]
+	_, ok2 := g.adjacencyMatrix[b]
+	if !ok1 || !ok2 {
 		return ErrNodeDoesNotExist
 	}
-	// connect the nodes providing a weight value
-	g.adjacencyMatrix[a][b] = weight
+	g.adjacencyMatrix[a][b] = weight // connect the nodes providing a weight value
 	return nil
+}
+
+// Neighbours gets neighbours of a node. specifically the nodes you can go to from node a.
+func (g *Graph) Neighbours(a Node) []Node {
+	neighbours := make([]Node, len(g.adjacencyMatrix[a]))
+	i := 0
+	for key := range g.adjacencyMatrix[a] {
+		neighbours[i] = key
+		i++
+	}
+	return neighbours
+}
+
+// Exists returns true if the node exists in the graph
+func (g *Graph) Exists(a Node) bool {
+	return false
 }
 
 // Represent outputs a human readable form of graph to stdout
@@ -47,7 +71,28 @@ func (g *Graph) Represent() {
 	fmt.Print(g.adjacencyMatrix)
 }
 
-// DFS performs a depth first search of the graph from node A to B. returns slice of path as well as cost. nil, -1 if no path found
-func (g *Graph) DFS(a Node, b Node) ([]Node, int) {
-	return nil, 0
+// DFS performs a DFS on the graph and returns slice of Nodes in the order they were found during
+// the search
+func (g *Graph) DFS(a Node) []Node {
+	visited := map[Node]bool{}
+	found := []Node{}
+	stack := []Node{}
+	curr := a
+	visited[curr] = true
+	found = append(found, curr)
+	for _, neighbour := range g.Neighbours(curr) {
+		stack = append(stack, neighbour)
+	}
+	for len(stack) != 0 {
+		curr, stack = stack[len(stack)-1], stack[:len(stack)-1] // pop
+		if visited[curr] == true {
+			continue
+		}
+		visited[curr] = true
+		found = append(found, curr)
+		for _, neighbour := range g.Neighbours(curr) {
+			stack = append(stack, neighbour)
+		}
+	}
+	return found
 }
--- pkg/graphs/graphs_test.go
+++ pkg/graphs/graphs_test.go
@@ -1,20 +1,164 @@
 package graphs
 
 import (
+	"fmt"
 	"testing"
 )
 
-// TestGraphCreate is responsible for testing the initialization of the graph
-func TestGraphCreate(t *testing.T) {
+// TestGraphInsert is responsible for testing Insertion of elements in the graph
+func TestGraphInsert(t *testing.T) {
 	// TODO: use testing package
-	graph := Graph{}
-	graph.Init()
-	graph.Insert("A")
-	graph.Insert("B")
-	graph.Insert("C")
-	graph.Connect("A", "B", 1)
-	graph.Connect("A", "C", 100)
-	graph.Connect("B", "C", -1)
-	graph.Connect("C", "B", 1)
-	graph.Represent()
+	var tests = []int{
+		1,
+		10,
+		100,
+		1_000,
+		10_000,
+		100_000,
+		1_000_000,
+		10_000_000,
+	}
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%d", tt)
+		t.Run(testName, func(t *testing.T) {
+			g := new(Graph)
+			g.Init()
+			for i := 0; i < tt; i++ {
+				g.Insert(i)
+			}
+		})
+	}
+}
+
+func TestGraphConnect(t *testing.T) {
+	g := Graph{}
+	g.Init()
+	for i := 0; i < 10; i++ {
+		g.Insert(i)
+	}
+	var tests = []struct {
+		a    Node
+		b    Node
+		w    int
+		want error
+	}{
+		{1, 2, 1, nil},
+		{-1, 2, 1, ErrNodeDoesNotExist},
+		{1, -1, 1, ErrNodeDoesNotExist},
+		{1, 9, 0, nil},
+		{2, 1, 100, nil},
+		{1, 1, 1, nil},
+		{1, 3, 1, nil},
+		{1, 3, -1, ErrNegativeEdgeWeight},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%d, %d, %d, %e", tt.a, tt.b, tt.w, tt.want)
+		t.Run(testName, func(t *testing.T) {
+			actual := g.Connect(tt.a, tt.b, tt.w)
+			if actual != tt.want {
+				t.Errorf("want %e got %e", tt.want, actual)
+			}
+
+		})
+	}
+}
+
+func TestGraphNeighbours(t *testing.T) {
+	g := Graph{}
+	g.Init()
+	for i := 0; i < 10; i++ {
+		g.Insert(i)
+	}
+	g.Connect(1, 2, 1)
+	g.Connect(1, 3, 7)
+	g.Connect(5, 4, 1)
+	g.Connect(4, 2, 8)
+	g.Connect(15, 1, 4)
+
+	tests := []struct {
+		a    Node
+		want []Node
+	}{
+		{1, []Node{2, 3}},
+		{2, []Node{}},
+		{3, []Node{}},
+		{4, []Node{2}},
+		{5, []Node{4}},
+		{6, []Node{}},
+		{7, []Node{}},
+		{8, []Node{}},
+		{9, []Node{}},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%d", tt.a)
+		t.Run(testName, func(t *testing.T) {
+			actual := g.Neighbours(tt.a)
+			if actual == nil && tt.want != nil {
+				t.Errorf("want %t got %t", tt.want, actual)
+			} else if actual != nil && tt.want == nil {
+				t.Errorf("want %t got %t", tt.want, actual)
+			} else if actual != nil && tt.want != nil {
+				if len(actual) != len(tt.want) {
+					t.Errorf("want %t got %t", tt.want, actual)
+				}
+				for i, v := range tt.want {
+					if v != actual[i] {
+						t.Errorf("want %t got %t", tt.want, actual)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestGraphDFS(t *testing.T) {
+	g := Graph{}
+	g.Init()
+
+	g.Insert("A")
+	g.Insert("B")
+	g.Insert("C")
+	g.Insert("D")
+	g.Insert("E")
+	g.Insert("F")
+
+	g.Connect("A", "B", 1)
+	g.Connect("A", "E", 1)
+	g.Connect("B", "C", 1)
+	g.Connect("B", "D", 1)
+	g.Connect("C", "A", 1)
+	g.Connect("C", "F", 1)
+	g.Connect("D", "F", 1)
+	g.Connect("E", "C", 1)
+	g.Connect("F", "E", 1)
+
+	tests := []struct {
+		a    Node
+		want []Node
+	}{
+		{"A", []Node{"A", "E", "C", "F", "B", "D"}},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%t", tt.a)
+		t.Run(testName, func(t *testing.T) {
+			actual := g.DFS("A")
+			if actual == nil && tt.want != nil {
+				t.Errorf("want %t got %t", tt.want, actual)
+			} else if actual != nil && tt.want == nil {
+				t.Errorf("want %t got %t", tt.want, actual)
+			} else if actual != nil && tt.want != nil {
+				if len(actual) != len(tt.want) {
+					t.Errorf("want %t got %t", tt.want, actual)
+				}
+				for i, v := range tt.want {
+					if v != actual[i] {
+						t.Errorf("want %t got %t", tt.want, actual)
+					}
+				}
+			}
+		})
+	}
 }
