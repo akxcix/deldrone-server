75303c802d8b3ed1596e5c18eee1482f3f4a7912
Adarsh Kumar <contactadarshk@gmail.com>
2020-11-15 04:36:10 +0530

    add Dijkstra and corresponding test

--- pkg/graphs/graphs.go
+++ pkg/graphs/graphs.go
@@ -1,8 +1,13 @@
 package graphs
 
+// TODO: Rewrite. Works rn but can be optimized a lot
+
 import (
+	"container/heap"
 	"errors"
-	"fmt"
+	"math"
+
+	"github.com/iamadarshk/deldrone-server/internal/pqueue"
 )
 
 var (
@@ -12,10 +17,25 @@ var (
 
 	// ErrNegativeEdgeWeight is used when the graph is provided a negative edge weight
 	ErrNegativeEdgeWeight = errors.New("graphs: negative edge weight in graph")
+
+	// ErrUnreachableNode is used when a pathfinding algorithm isn't able to reach destination node
+	ErrUnreachableNode = errors.New("graphs: the destination node is unreachable")
+)
+
+var (
+	// +Inf
+	inf = math.MaxInt64
 )
 
 // Node is an empty interface that represents any item that can be put into the graph
 type Node interface{}
+
+// PathNode is a container used to hold the predecessor of the current node through which the
+// cost of traversal is minimized
+type PathNode struct {
+	contained Node
+	via       Node
+}
 
 // Graph implements a directed weighted graph using an adjacency matrix using nested hashmaps
 type Graph struct {
@@ -49,6 +69,19 @@ func (g *Graph) Connect(a Node, b Node, weight int) error {
 	return nil
 }
 
+// ConnectBoth connects a to b and b to a with weight
+func (g *Graph) ConnectBoth(a, b Node, weight int) error {
+	err := g.Connect(a, b, weight)
+	if err != nil {
+		return err
+	}
+	err = g.Connect(b, a, weight)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // Neighbours gets neighbours of a node. specifically the nodes you can go to from node a.
 func (g *Graph) Neighbours(a Node) []Node {
 	neighbours := make([]Node, len(g.adjacencyMatrix[a]))
@@ -60,20 +93,30 @@ func (g *Graph) Neighbours(a Node) []Node {
 	return neighbours
 }
 
+// All returns list of all nodes that are present in the graph
+func (g Graph) All() []Node {
+	all := make([]Node, len(g.adjacencyMatrix))
+	i := 0
+	for key := range g.adjacencyMatrix {
+		all[i] = key
+		i++
+	}
+	return all
+}
+
 // Exists returns true if the node exists in the graph
 func (g *Graph) Exists(a Node) bool {
 	return false
 }
 
-// Represent outputs a human readable form of graph to stdout
-// TODO: complete function
-func (g *Graph) Represent() {
-	fmt.Print(g.adjacencyMatrix)
-}
-
 // DFS performs a DFS on the graph and returns slice of Nodes in the order they were found during
 // the search
-func (g *Graph) DFS(a Node) []Node {
+func (g *Graph) DFS(a Node) ([]Node, error) {
+	_, ok := g.adjacencyMatrix[a]
+	if !ok {
+		return nil, ErrNodeDoesNotExist
+	}
+
 	visited := map[Node]bool{}
 	found := []Node{}
 	stack := []Node{}
@@ -94,5 +137,114 @@ func (g *Graph) DFS(a Node) []Node {
 			stack = append(stack, neighbour)
 		}
 	}
-	return found
+	return found, nil
+}
+
+// Dijkstra provides the shortest path and its cost in a graph from a to b. returns
+// error if it's not possible to reach
+func (g *Graph) Dijkstra(source, destination Node) ([]Node, int, error) {
+	// check if both nodes exist in graph
+	_, ok1 := g.adjacencyMatrix[source]
+	_, ok2 := g.adjacencyMatrix[destination]
+	if !ok1 || !ok2 {
+		return nil, 0, ErrNodeDoesNotExist
+	}
+
+	pq := pqueue.PQueue{}      // pq maintains order based on cost
+	heap.Init(&pq)             // init pq
+	visited := map[Node]bool{} // keeps track of node we have already visited
+	all := g.All()             // get slice of all nodes
+	processed := []pqueue.Item{}
+
+	// add all nodes to pq
+	for _, node := range all { // range over all the nodes in the graph
+		if node == source {
+			// if the node is source node, set priority 0 and predecessor nil
+			heap.Push(&pq, pqueue.Item{
+				Value: PathNode{
+					contained: source,
+					via:       nil,
+				},
+				Priority: 0,
+			})
+		} else {
+			// otherwise set priority = infinity, predecessor = source
+			heap.Push(&pq, pqueue.Item{
+				Value: PathNode{
+					contained: node,
+					via:       source,
+				},
+				Priority: inf,
+			})
+		}
+	}
+
+	curr := pq[0] // var curr holds the node which is being processed
+	for {
+		currConatained := curr.Value.(PathNode).contained
+		// if we have already visited the node, skip
+		if visited[currConatained] == true {
+			curr = heap.Pop(&pq).(pqueue.Item)
+			continue
+		}
+
+		// else compute distance for each node. if it''s less, update
+		neighbours := g.Neighbours(currConatained)
+		for _, neighbour := range neighbours {
+			for i := 0; i < pq.Len(); i++ {
+				other := pq[i].Value.(PathNode).contained
+				if neighbour != other {
+					continue
+				}
+				if curr.Priority+g.adjacencyMatrix[currConatained][other] < pq[i].Priority {
+					// update pq if a shorter path is found
+					pq[i] = pqueue.Item{
+						Value: PathNode{
+							contained: other,
+							via:       currConatained,
+						},
+						Priority: curr.Priority + g.adjacencyMatrix[currConatained][other],
+					}
+					heap.Init(&pq) // fix order of pq
+				}
+			}
+		}
+
+		visited[currConatained] = true      // mark node as visited
+		processed = append(processed, curr) // copy processed node to a different location
+
+		// break out if destination is reached
+		if currConatained == destination {
+			break
+		}
+
+		curr = heap.Pop(&pq).(pqueue.Item)
+	}
+
+	pathMap := map[Node]Node{} // maps a node to its predecessor in the shortest path
+	costMap := map[Node]int{}  // maps a node to the cost of the path required to reach it
+
+	// populate pathMap and costMap
+	for _, pathNode := range processed {
+		node := pathNode.Value.(PathNode).contained
+		pathMap[node] = pathNode.Value.(PathNode).via
+		costMap[node] = pathNode.Priority
+	}
+
+	// return error if cost == inf
+	if costMap[destination] == inf {
+		return nil, 0, ErrUnreachableNode
+	}
+
+	path := []Node{}                 // holds the shortest path
+	path = append(path, destination) // append destination to list
+	prev := pathMap[destination]     // store it's predecessor
+	for prev != nil {                // while predecessor to current node exists
+		path = append(path, prev) // add that predecessor
+		prev = pathMap[prev]      // update predecessor
+	}
+
+	cost := costMap[destination] // find cost to reach destination
+
+	return path, cost, nil
 }
--- pkg/graphs/graphs_test.go
+++ pkg/graphs/graphs_test.go
@@ -2,8 +2,27 @@ package graphs
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
+
+// helper function to check whether two slices are equal
+func checkEqualNodeSlice(actual []Node, want []Node, t *testing.T) {
+	if actual == nil && want != nil {
+		t.Errorf("want %v got %v", want, actual)
+	} else if actual != nil && want == nil {
+		t.Errorf("want %v got %v", want, actual)
+	} else if actual != nil && want != nil {
+		if len(actual) != len(want) {
+			t.Errorf("want %v got %v", want, actual)
+		}
+		for i, v := range want {
+			if v != actual[i] {
+				t.Errorf("want %v got %v", want, actual)
+			}
+		}
+	}
+}
 
 // TestGraphInsert is responsible for testing Insertion of elements in the graph
 func TestGraphInsert(t *testing.T) {
@@ -16,7 +35,6 @@ func TestGraphInsert(t *testing.T) {
 		10_000,
 		100_000,
 		1_000_000,
-		10_000_000,
 	}
 	for _, tt := range tests {
 		testName := fmt.Sprintf("%d", tt)
@@ -52,12 +70,46 @@ func TestGraphConnect(t *testing.T) {
 		{1, 3, -1, ErrNegativeEdgeWeight},
 	}
 
-	for _, tt := range tests {
-		testName := fmt.Sprintf("%d, %d, %d, %e", tt.a, tt.b, tt.w, tt.want)
+	for i, tt := range tests {
+		testName := fmt.Sprintf("%d", i)
 		t.Run(testName, func(t *testing.T) {
 			actual := g.Connect(tt.a, tt.b, tt.w)
 			if actual != tt.want {
-				t.Errorf("want %e got %e", tt.want, actual)
+				t.Errorf("want %v got %v", tt.want, actual)
+			}
+
+		})
+	}
+}
+
+func TestGraphConnectBoth(t *testing.T) {
+	g := Graph{}
+	g.Init()
+	for i := 0; i < 10; i++ {
+		g.Insert(i)
+	}
+	var tests = []struct {
+		a    Node
+		b    Node
+		w    int
+		want error
+	}{
+		{1, 2, 1, nil},
+		{-1, 2, 1, ErrNodeDoesNotExist},
+		{1, -1, 1, ErrNodeDoesNotExist},
+		{1, 9, 0, nil},
+		{2, 1, 100, nil},
+		{1, 1, 1, nil},
+		{1, 3, -1, ErrNegativeEdgeWeight},
+		{1, 3, 1, nil},
+	}
+
+	for i, tt := range tests {
+		testName := fmt.Sprintf("%d", i)
+		t.Run(testName, func(t *testing.T) {
+			actual := g.ConnectBoth(tt.a, tt.b, tt.w)
+			if actual != tt.want {
+				t.Errorf("want %v got %v", tt.want, actual)
 			}
 
 		})
@@ -95,20 +147,44 @@ func TestGraphNeighbours(t *testing.T) {
 		testName := fmt.Sprintf("%d", tt.a)
 		t.Run(testName, func(t *testing.T) {
 			actual := g.Neighbours(tt.a)
-			if actual == nil && tt.want != nil {
-				t.Errorf("want %t got %t", tt.want, actual)
-			} else if actual != nil && tt.want == nil {
-				t.Errorf("want %t got %t", tt.want, actual)
-			} else if actual != nil && tt.want != nil {
-				if len(actual) != len(tt.want) {
-					t.Errorf("want %t got %t", tt.want, actual)
-				}
-				for i, v := range tt.want {
-					if v != actual[i] {
-						t.Errorf("want %t got %t", tt.want, actual)
-					}
-				}
-			}
+			checkEqualNodeSlice(actual, tt.want, t)
+		})
+	}
+}
+
+func TestGraphAll(t *testing.T) {
+	g := Graph{}
+	g.Init()
+	for i := 0; i < 10; i++ {
+		g.Insert(i)
+	}
+	g.Connect(1, 2, 1)
+	g.Connect(1, 3, 7)
+	g.Connect(5, 4, 1)
+	g.Connect(4, 2, 8)
+	g.Connect(15, 1, 4)
+
+	tests := []struct {
+		want []Node
+	}{
+		{[]Node{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("all")
+		t.Run(testName, func(t *testing.T) {
+			actual := g.All()
+
+			sort.Slice(actual, func(i, j int) bool {
+				return actual[i].(int) < actual[j].(int)
+			})
+
+			sort.Slice(tt.want, func(i, j int) bool {
+				return tt.want[i].(int) < tt.want[j].(int)
+			})
+
+			checkEqualNodeSlice(actual, tt.want, t)
+
 		})
 	}
 }
@@ -135,30 +211,72 @@ func TestGraphDFS(t *testing.T) {
 	g.Connect("F", "E", 1)
 
 	tests := []struct {
-		a    Node
-		want []Node
+		a       Node
+		want    []Node
+		wantErr error
 	}{
-		{"A", []Node{"A", "E", "C", "F", "B", "D"}},
+		{"A", []Node{"A", "E", "C", "F", "B", "D"}, nil},
+		{"G", nil, ErrNodeDoesNotExist},
 	}
 
 	for _, tt := range tests {
-		testName := fmt.Sprintf("%t", tt.a)
+		testName := fmt.Sprintf("%v", tt.a)
 		t.Run(testName, func(t *testing.T) {
-			actual := g.DFS("A")
-			if actual == nil && tt.want != nil {
-				t.Errorf("want %t got %t", tt.want, actual)
-			} else if actual != nil && tt.want == nil {
-				t.Errorf("want %t got %t", tt.want, actual)
-			} else if actual != nil && tt.want != nil {
-				if len(actual) != len(tt.want) {
-					t.Errorf("want %t got %t", tt.want, actual)
-				}
-				for i, v := range tt.want {
-					if v != actual[i] {
-						t.Errorf("want %t got %t", tt.want, actual)
-					}
-				}
+			actual, actualErr := g.DFS(tt.a)
+			if actualErr != tt.wantErr {
+				t.Errorf("want %v got %v", tt.wantErr, actualErr)
 			}
+			checkEqualNodeSlice(actual, tt.want, t)
+		})
+	}
+}
+
+func TestGraphDijkstra(t *testing.T) {
+	g := Graph{}
+	g.Init()
+
+	g.Insert("A")
+	g.Insert("B")
+	g.Insert("C")
+	g.Insert("D")
+	g.Insert("E")
+	g.Insert("F")
+	g.Insert("G")
+
+	g.Connect("A", "B", 1)
+	g.Connect("A", "E", 2)
+	g.Connect("B", "C", 3)
+	g.Connect("B", "D", 4)
+	g.Connect("C", "A", 5)
+	g.Connect("C", "F", 6)
+	g.Connect("D", "F", 7)
+	g.Connect("E", "C", 8)
+	g.Connect("F", "E", 9)
+
+	tests := []struct {
+		a         Node
+		b         Node
+		wantSlice []Node
+		wantCost  int
+		wantErr   error
+	}{
+		{"A", "F", []Node{"F", "C", "B", "A"}, 10, nil},
+		{"C", "D", []Node{"D", "B", "A", "C"}, 10, nil},
+		{"A", "G", nil, 0, ErrUnreachableNode},
+		{"A", "I", nil, 0, ErrNodeDoesNotExist},
+	}
+
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%s to %s", tt.a, tt.b)
+		t.Run(testName, func(t *testing.T) {
+			actualSlice, actualCost, actualErr := g.Dijkstra(tt.a, tt.b)
+			if actualErr != tt.wantErr {
+				t.Errorf("want %v got %v", tt.wantErr, actualErr)
+			}
+			if actualCost != tt.wantCost {
+				t.Errorf("want %v got %v", tt.wantCost, actualCost)
+			}
+			checkEqualNodeSlice(actualSlice, tt.wantSlice, t)
 		})
 	}
 }
